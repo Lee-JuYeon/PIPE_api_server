adce1f58842342a3eebd0df76d8d09336daeeb81
Lee-JuYeon <cavss1118@gmail.com>
2022-11-02 14:26:50 +0900

    git ignore

---
+++ auth-middleware.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    // if(req.path == '/users'){
+    //     next()
+    // }else{
+    //     res.status(400).json({
+    //         err : "test error 못지나간다"
+    //     });
+    // }
+}
